e4ea3b8954830e6670700fd7d53919e3804e66e9
br1 <delgado.hebru24@triana.salesianos.edu>
2025-02-21 13:08:18 +0100

    Falta el activate

--- lagrada/src/main/java/com/triana/salesianos/dam/lagrada/controller/UserController.java
+++ lagrada/src/main/java/com/triana/salesianos/dam/lagrada/controller/UserController.java
@@ -13,6 +13,7 @@ import com.triana.salesianos.dam.lagrada.service.UserService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PostAuthorize;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
