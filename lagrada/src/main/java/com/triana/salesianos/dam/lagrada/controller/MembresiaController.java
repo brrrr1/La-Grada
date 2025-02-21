627314ea9840ea35f95ddc3fb6e968d10e1d4241
br1 <delgado.hebru24@triana.salesianos.edu>
2025-02-21 14:20:43 +0100

    He puesto un put como get, falta todavia hacer el controlador, documentar los métodos con swagger, y añadir restricción de rol para los endpoint en el security config

--- lagrada/src/main/java/com/triana/salesianos/dam/lagrada/controller/MembresiaController.java
+++ lagrada/src/main/java/com/triana/salesianos/dam/lagrada/controller/MembresiaController.java
@@ -1,9 +1,40 @@
 package com.triana.salesianos.dam.lagrada.controller;
 
+import com.triana.salesianos.dam.lagrada.service.MembresiaService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.UUID;
 
 @RestController
 @RequiredArgsConstructor
 public class MembresiaController {
+
+    private final MembresiaService membresiaService;
+
+    // hacer el controlador, documentar los métodos con swagger, y añadir restricción de rol para los endpoint en el security config
+    //Métodos para editar precio de una membresia, obtener todas, obtener una
+
+    //Metodo para editar precio de una membresia
+    @PutMapping("/membresia/edit/{id}/{precio}")
+    public void editMembresiaPrice(UUID id, Double precio) {
+        membresiaService.editMembresiaPrice(id, precio);
+    }
+
+
+    //Metodo para obtener todas las membresias
+    @GetMapping("/membresia/all")
+    public void findAll() {
+        membresiaService.findAll();
+    }
+
+    //Metodo para obtener una membresia
+    @GetMapping("/membresia/{id}")
+    public void findById(UUID id) {
+        membresiaService.findById(id);
+    }
+
+
 }
