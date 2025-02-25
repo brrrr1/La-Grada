b8313845c0bf81d6bfb7db687ef795dc5fdde5fa
br1 <delgado.hebru24@triana.salesianos.edu>
2025-02-25 10:48:11 +0100

    Añadidos los permisos para los endpoints

--- lagrada/src/main/java/com/triana/salesianos/dam/lagrada/security/SecurityConfig.java
+++ lagrada/src/main/java/com/triana/salesianos/dam/lagrada/security/SecurityConfig.java
@@ -66,12 +66,33 @@ public class SecurityConfig {
         /*http.authorizeHttpRequests(authz -> authz
                 .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login", "/auth/refresh/token","activate/account", "/error").permitAll()
                 .requestMatchers("/me/admin").hasRole("ADMIN")
+                .requestMatchers("/me", "/auth/logout").authenticated()
                 .requestMatchers("/h2-console/**").permitAll()
                 .anyRequest().authenticated());*/
 
-        //Provisional para que se pueda acceder a todo
         http.authorizeHttpRequests(authz -> authz
-                .anyRequest().permitAll());
+                .requestMatchers(HttpMethod.POST, "/auth/register", "/auth/login", "/auth/refresh/token", "/activate/account", "/error").permitAll()
+                .requestMatchers("/me/admin").hasRole("ADMIN") // Solo los admin pueden acceder
+                .requestMatchers("/me", "/auth/logout").authenticated() // Solo los usuarios autenticados pueden acceder
+                .requestMatchers("/h2-console/**").permitAll() // Permite el acceso al H2 console
+                .requestMatchers(HttpMethod.POST, "/user/choose-favorite-team/{teamId}", "/user/change-favorite-team/{newTeamId}", "/user/buy-ticket/{eventId}")
+                .authenticated() // Solo los usuarios autenticados pueden comprar entradas, cambiar equipo favorito
+                .requestMatchers("/user/edit-info", "/user/edit-password").authenticated() // Usuarios autenticados pueden editar info y contraseña (PUT)
+                .requestMatchers("/user/favorite-team-events", "/eventos-futuros", "/eventos-pasados")
+                .authenticated() // Solo los usuarios autenticados pueden ver estos eventos
+                .requestMatchers("/equipo/**").hasRole("ADMIN") // Solo los administradores pueden crear, editar y borrar equipos (POST, PUT, DELETE)
+                .requestMatchers(HttpMethod.GET, "/equipo/**").permitAll() // Todos pueden ver los equipos (GET)
+                .requestMatchers("/evento/**").hasRole("ADMIN") // Solo los admin pueden crear, editar y borrar eventos (POST, PUT, DELETE)
+                .requestMatchers(HttpMethod.GET, "/evento/proximos").permitAll() // Todos pueden ver los eventos futuros (GET)
+                .anyRequest().authenticated()); // El resto de las peticiones requieren autenticación
+
+
+
+
+
+        //Provisional para que se pueda acceder a t0d0
+        /*http.authorizeHttpRequests(authz -> authz
+                .anyRequest().permitAll());*/
 
 
 
